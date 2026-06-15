7b660c96ddec7227e3971f9c4aac9deefc8b75d2
suganyas2382006-sketch <suganyas2382006@gmail.com>
2026-06-15 21:05:10 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,25 +1,59 @@
-import os
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-DATASET_PATH = "dataset/dataset"
+train_dir = "dataset/Training"
+test_dir = "dataset/Testing"
 
-IMG_SIZE = (224, 224)
-BATCH_SIZE = 32
+img_size = (128, 128)
+batch_size = 16
 
-train_datagen = ImageDataGenerator(
-    rescale=1.0 / 255,
-    validation_split=0.2,
-    rotation_range=10,
-    zoom_range=0.2,
-    horizontal_flip=True
-)
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_data = train_datagen.flow_from_directory(
-    DATASET_PATH,
-    target_size=IMG_SIZE,
+    train_dir,
+    target_size=img_size,
+    batch_size=batch_size,
+    class_mode="categorical"
+)
+
+test_data = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=img_size,
+    batch_size=batch_size,
+    class_mode="categorical"
+)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(128, 128, 3)),
+
+    tf.keras.layers.Conv2D(32, (3,3), activation="relu"),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Conv2D(64, (3,3), activation="relu"),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Flatten(),
+
+    tf.keras.layers.Dense(64, activation="relu"),
+    tf.keras.layers.Dense(4, activation="softmax")
+])
+
+model.compile(
+    optimizer="adam",
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.fit(
+    train_data,
+    validation_data=test_data,
+    epochs=5
+)
+
+model.save("model/brain_tumor_model.h5")
+
+print("Model saved successfully!")
     batch_size=BATCH_SIZE,
     class_mode="categorical",
     subset="training",
